9d5f5012783e3a2d133b0dac8959a4725c36c280
Felipe Parra <fpr29.10@gmail.com>
2023-09-12 09:19:27 -0600

    Fix memoization function name error

--- main.js
+++ main.js
@@ -197,6 +197,6 @@ function fibonacciWithMemo(n, memo) {
   memo = memo || {};
   if (memo[n]) return memo[n];
   if (n <= 1) return n;
-  return (memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo));
+  return (memo[n] =
+    fibonacciWithMemo(n - 1, memo) + fibonacciWithMemo(n - 2, memo));
 }
-console.log(fibonacciWithMemo(10));
